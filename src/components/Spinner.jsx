1e05e544b6d21b258b0752a525a7d98a34fa6e03
SHASHANK2405 <shashankgulati2405@gmail.com>
2023-11-06 00:16:19 +0530

    shop app

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className='flex justify-center items-center bottom-48'>
+      <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
---
+++ src/redux/Store.js
@@ -0,0 +1,18 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { CartSlice } from "./Slices/CartSlice";
+
+// export const store = configureStore({
+//     reducer:{
+//         cart: CartSlice.reducer,
+//     }
+// });
+
+import { configureStore } from "@reduxjs/toolkit";
+import CartSlice from "./Slices/CartSlice";
+
+
+export const store = configureStore({
+    reducer:{
+        cart: CartSlice
+    }
+});
